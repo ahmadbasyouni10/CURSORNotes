7fd6029119668b41bd2f2024eb92085a3f2532d2
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-12-31 01:39:33 -0500

    Styling

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
       <main className="">
-        <h1>CURSOR NOTES</h1>
+        <h1 className="2xl">CURSOR NOTES</h1>
         <Button>Click Me</Button>
       </main>
   )}
