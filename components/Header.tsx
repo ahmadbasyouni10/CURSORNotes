4f7b3c2853323c9e91589f2ed453daeda0b65925
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-12-31 01:39:12 -0500

    Header component/auth

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,27 @@
-function header() {
+'use client'
+import { useUser, SignedOut, SignInButton, SignedIn, UserButton} from "@clerk/nextjs";
+
+function Header() {
+  const { user } = useUser()
   return (
-    <div>header</div>
+    <div className="flex items-center justify-between p-5">
+      {user && (
+        <h1 className="text-2xl">{user.firstName}{`'s`} Space</h1>
+      )}
+
+      {/* Bread Crumbs */}
+
+      <div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </div>
   )
 }
 
-export default header
+export default Header
