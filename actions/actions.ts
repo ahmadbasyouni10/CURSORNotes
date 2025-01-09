c8ed205e11fb0b85f714748ab527685d5ed23443
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2025-01-09 02:28:24 -0500

    Add document to user collection, into their room collection

--- actions/actions.ts
+++ actions/actions.ts
@@ -10,12 +10,22 @@ export async function createNewDocument() {
     const { sessionClaims } = await auth();
 
     const collectionRef = adminDb.collection("documents");
+    // add a new document to the document collections
     const newDocRef = await collectionRef.add({
         title: "New document"
     })
 
-    await adminDb.collection("users").doc(sessionClaims?.email!)
+    // add the document to the user collection
+    // into their rooms collection
+    // this is a subcollection
+    // role is owner because they created the document
+    await adminDb.collection("users").doc(sessionClaims?.email!).collection("rooms").doc(newDocRef.id).set({
+        // INDEX query requires to be in the document
+        userId: sessionClaims?.email,
+        role: "owner",
+        createdAt: new Date(),
+        roomId: newDocRef.id
+    })
 
-
-    
+    return { docId: newDocRef.id }
 }
