ddd819dece416a723d61c945109a89cbb2b3555c
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2025-01-08 22:13:57 -0500

    useTransition hook for creating new doc in client side comp

--- components/NewDocumentButton.tsx
+++ components/NewDocumentButton.tsx
@@ -1,9 +1,26 @@
+'use client';
+
 import { Button } from "./ui/button";
+import { useTransition } from "react";
+import { useRouter } from "next/router";
+import { createNewDocument } from "../actions/actions";
 
 function NewDocumentButton() {
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+  const handleCreateNewDocument = () => {
+    startTransition(async() => {
+      // createnewdocument is a server action we will create later
+      // it will create a new document in the database
+      // and return the id of the new document
+      const { docId } = await createNewDocument();
+      // navigate to the new document
+      router.push(`/document/${docId}`);
+  })
+}
   return (
     <div className="p-2">
-        <Button>New Document</Button>
+        <Button onClick={handleCreateNewDocument} disabled={isPending}>{isPending ? "Creating..." : "New Document"}</Button>
     </div>
   )
 }
