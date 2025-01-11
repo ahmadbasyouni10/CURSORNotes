81d4842e3ebd2f823c6b31d98a9c57a1c8666ac3
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2025-01-10 20:53:14 -0500

    Packages and sessionclaims extended into JWT, so auth() returns session

--- globals.d.ts
+++ globals.d.ts
@@ -2,5 +2,7 @@ import { User } from "./types/types";
 
 
 declare global {
-    interface CustomJwtSessionClaims extends User {//for adding extra claims to the jwt later}
+    interface CustomJwtSessionClaims extends User {
+        //for adding extra claims to the jwt later
+    }
 }
