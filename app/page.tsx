3b9755711036d8e9cf5d25d687579a36685e0b70
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2025-01-10 20:30:12 -0500

    Home page styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button"
+import { ArrowLeft } from "lucide-react";
 
 export default function Home() {
   return (
-      <main className="">
-        <h1 className="2xl">CURSOR NOTES</h1>
-        <Button>Click Me</Button>
+      <main className="p-2 flex items-center">
+        <ArrowLeft className="w-8 h-8" />
+        <h1 className="2xl font-bold">Get started with CursorNotes</h1>
       </main>
   )}
